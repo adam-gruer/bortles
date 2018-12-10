4a11606511af3d4917346ac5ff7849dcfbba6f09
Adam Gruer <adamgruer@icloud.com>
2018-12-10 22:02:20 +1100

    Add hook function to copy gifs to temp dir when package is loaded

---
+++ R/onload.R
@@ -0,0 +1,13 @@
+.onLoad <- function(...){
+  bortles_temp_dir2341 <<- tempfile()
+
+  dir <- bortles_temp_dir2341
+  dir.create(dir)
+  gif_files <- system.file(package = "bortles",
+                           paste0("fork",
+                                  1:7,
+                                  ".gif"))
+  file.copy(gif_files,
+            dir,
+            overwrite = TRUE)
+}
