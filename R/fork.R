a3c81b7a8a2bcf59a204841a2420d41e25f0e9b3
Adam Gruer <adamgruer@icloud.com>
2018-12-10 22:03:09 +1100

    Update fork function and documentation

--- R/fork.R
+++ R/fork.R
@@ -6,6 +6,14 @@
 #' @export
 #'
 #' @examples
+#' fork()
 fork <- function(){
 
+gif <- sample(1:7,1)
+rstudioapi::viewer(paste0(bortles_temp_dir2341,"/","fork",gif,".gif"))
+
+#file.remove(file.path(dir, list.files(dir)))
+
 }
+
+
