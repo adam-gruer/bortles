3fdff06c971f154dd4a3ea988a44239f70739389
Adam Gruer <adamgruer@icloud.com>
2018-12-11 22:48:52 +1100

    Add hex sticker and code to create it

---
+++ inst/figures/hex.R
@@ -0,0 +1,11 @@
+
+hexSticker::sticker("inst/figures/molotov.png",
+                    package="bortles",
+                    # p_size=6,
+
+                     s_x=1.1,
+                    s_y=.83,
+                     s_width=.48,
+                     s_height = 0.6,
+                    h_fill = "#41803C",
+                    filename="inst/figures/imgfile.png")
